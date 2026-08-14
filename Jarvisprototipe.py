1075e58eff5a596a7ae1dc0f0de5a440555e6d47
elpit08 <pgdr250920@gmail.com>
2026-08-14 04:22:55 +0000

    the real jarvisprototipe

--- Jarvisprototipe.py
+++ Jarvisprototipe.py
@@ -1,5 +1,9 @@
-def main():
+def talk_to_jarvis(mensaje):
+    url = "http://localhost:11434/api/generate"
 
 
-    if __name__ == "__main__":
-        main()
+    data = {
+        "model": "granite4.1:8b",
+        "prompt": mensaje,
+        "stream": False
+    }
