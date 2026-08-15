804335eab9fb34a7561cabfadf5296caf3e3fce4
elpit08 <pgdr250920@gmail.com>
2026-08-15 00:41:31 +0000

    Maybe the final prototype

--- Jarvisprototipe.py
+++ Jarvisprototipe.py
@@ -7,3 +7,20 @@ def talk_to_jarvis(mensaje):
         "prompt": mensaje,
         "stream": False
     }
+    anwser = requests.post(url,json=data)
+    anwser_json = anwser.json()
+    anwser_text = anwser_json["response"]
+
+    return anwser_text
+
+print("---Jarvis Prototipe---")
+print("Write `exit` to finish the sesion\n")
+
+while True:
+    my_message = input("Yo: ")
+
+    if my_message.lower() == "exit":
+        print("Jarvis is at your service")
+        break
+    anwser= talk_to_jarvis(my_message)
+    print(f"Jarvis: {anwser}\n")
